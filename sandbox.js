421c3ceb1b4b7ba98b1adba354d06d9b20133657
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-09-11 22:15:22 +0300

    1-st part

---
+++ sandbox.js
@@ -0,0 +1,178 @@
+// // * 7
+// const numbers = [51, 18, 13, 24, 7, 85, 19, 3, 1];
+
+// // * Poly,Mango,Ajax,Kiwi
+// // const friends = ["Poly", "Mango", "Ajax", "Kiwi"];
+
+// // * jAVAsCRIPT
+// const string = "JavaScript";
+
+// // * top-10-benefits-of-react-framework
+// const title = "Top 10 benefits of React framework";
+
+// // * Sum of array`s elements
+// const array1 = [5, 10, 15, 20];
+// const array2 = [10, 20, 35, 45];
+
+// // * Splice | Remove.Add.Update
+// const items = [51, 18, 13, 24, 7, 85, 19, 3];
+
+// function findFriendByName(allFriends, name) {
+//   for (const friend of allFriends) {
+//     if (friend.name === name) {
+//       console.log(`${name} is in your friendlist`);
+//       return;
+//     }
+//   }
+//   console.log(`${name} is not in your friendlist `);
+// }
+
+// console.log(findFriendByName(friends, "Poly"));
+// console.log(findFriendByName(friends, "Bobby"));
+
+// function getOnlineFriends(allFriends) {
+//   let onlineFriends = [];
+//   for (const friend of allFriends) {
+//     if (friend.online) {
+// onlineFriends.push(friend);
+//     }
+//   }
+
+//   return onlineFriends;
+// }
+// console.log(getOnlineFriends(friends));
+// const cart = {
+//   items: [],
+//   getItems() {
+//     return this.items;
+//   },
+//   add(product) {
+//     const newProduct = {
+//       ...product,
+//       quantity: 1,
+//     };
+//     for (const item of this.items) {
+//       if (product.name === item.name) {
+//         item.quantity += 1;
+//         return;
+//       }
+//     }
+
+//     this.items.push(newProduct);
+//   },
+//   remove(productName) {
+//     const { items } = this;
+
+//     for (const item of items) {
+//       if (item.name === productName) {
+//         const index = items.indexOf(item);
+//         items.splice(index, 1);
+//         break;
+//       }
+//     }
+//   },
+//   clear() {
+//     this.items = [];
+//   },
+//   countTotalPrice() {
+//     const { items } = this;
+
+//     let totalPrice = 0;
+//     for (const { price, quantity } of items) {
+//       totalPrice += Number(price) * quantity;
+//     }
+//     return totalPrice;
+//   },
+//   icnreaseQuantity() {},
+//   decreaseQuantity() {},
+// };
+
+// cart.add({ name: "apple", price: "50" });
+// cart.add({ name: "melon", price: "150" });
+// cart.add({ name: "banana", price: "250" });
+// cart.add({ name: "strawberry", price: "350" });
+
+// console.table(cart.getItems());
+
+// cart.remove("apple");
+// cart.add({ name: "strawberry", price: "350" });
+
+// console.table(cart.items);
+// console.table(cart.countTotalPrice());
+
+// const fnA = function (parameter) {
+//   const innerVariable = "Значення внутрішньої змінної функцї fnA";
+
+//   const innerFunction = function () {};
+// };
+// for (let index = 0; index < array.length; index++) {
+//   const element = arraSy[index];
+// }
+
+// const friends = [
+//   {
+//     name: "Mango",
+//     online: false,
+//     xp: 150,
+//     tags: ["js", "html"],
+//   },
+//   {
+//     name: "Kiwi",
+//     online: true,
+//     xp: 160,
+//     tags: ["js", "html", "node.js"],
+//   },
+//   {
+//     name: "Poly",
+//     online: false,
+//     xp: 140,
+//     tags: ["js", "css", "react"],
+//   },
+//   {
+//     name: "Ajax",
+//     online: true,
+//     xp: 100,
+//     tags: ["html", "css", "react"],
+//   },
+// ];
+
+// const totalXp = friends.reduce((total, friend) => total + friend.xp, 0);
+// const allTags = friends.reduce((acc, friend) => [...friend.tags, ...acc], []);
+// const statsTags = allTags.reduce(
+//   (acc, tag) => ({ ...acc, [tag]: acc[tag] ? acc[tag] + 1 : 1 }),
+//   {}
+// );
+
+// const onlineAndSorted = friends.filter((friend) => friend.online).sort((a, b) => a.xp - b.xp);
+// console.log(totalXp);
+// console.log(allTags);
+
+// console.log(statsTags);
+// console.log(onlineAndSorted);
+
+// const a = Math.PI;
+
+// console.log(Math);
+// function fakeBin(x) {
+//   const arr = x.split("");
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] >= 5) {
+//       arr[i] = 1;
+//     } else {
+//       arr[i] = 0;
+//     }
+//   }
+
+//   return arr;
+// }
+
+// console.log(fakeBin("45385593107843568"));
+
+// async function getPoki() {
+//   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`);
+//   const poki = await response.json();
+
+//   return poki.abilities;
+// }
+// console.log(await getPoki());
