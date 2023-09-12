da05ed53ce34c6c8b8c3467c2d37c82f4a1ae56d
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-09-12 13:45:10 +0300

    2nd part

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,20 @@
-// // * 7
+// * 7
 // const numbers = [51, 18, 13, 24, 7, 85, 19, 3, 1];
 
-// // * Poly,Mango,Ajax,Kiwi
-// // const friends = ["Poly", "Mango", "Ajax", "Kiwi"];
+// * Poly,Mango,Ajax,Kiwi
+// const friends = ["Poly", "Mango", "Ajax", "Kiwi"];
 
-// // * jAVAsCRIPT
+//  * jAVAsCRIPT
 // const string = "JavaScript";
 
-// // * top-10-benefits-of-react-framework
+//  * top-10-benefits-of-react-framework
 // const title = "Top 10 benefits of React framework";
 
-// // * Sum of array`s elements
+// * Sum of array`s elements
 // const array1 = [5, 10, 15, 20];
 // const array2 = [10, 20, 35, 45];
 
-// // * Splice | Remove.Add.Update
+// * Splice | Remove.Add.Update
 // const items = [51, 18, 13, 24, 7, 85, 19, 3];
 
 // function findFriendByName(allFriends, name) {
@@ -176,3 +176,85 @@
 //   return poki.abilities;
 // }
 // console.log(await getPoki());
+
+// function freezeObj() {
+//   const MATH_CONSTANTS = {
+//     PI: 3.14,
+//   };
+//   // Only change code below this line
+//   Object.freeze(MATH_CONSTANTS);
+//   // Only change code above this line
+//   try {
+//     MATH_CONSTANTS.PI = 99;
+//   } catch (ex) {
+//     console.log(ex);
+//   }
+//   return MATH_CONSTANTS.PI;
+// }
+// const PI = freezeObj();
+
+// console.log("PI", PI);
+
+// var magic = function () {
+//   return new Date();
+// };
+
+// const magic = () => new Date();
+
+// var myConcat = function (arr1, arr2) {
+//   return arr1.concat(arr2);
+// };
+
+// const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+// console.log(myConcat([1, 2], [3, 4, 5]));
+
+// const increment = (number = 1, value) => number + value;
+
+// const sum = (...args) => {
+//   let total = 0;
+//   for (let i = 0; i < args.length; i++) {
+//     total += args[i];
+//   }
+
+//   return total;
+// };
+
+// console.log(sum(1, 2, 3));
+
+// const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
+// let arr2;
+
+// arr2 = [...arr1]; // Change this line
+
+// console.log(arr2);
+
+// const HIGH_TEMPERATURES = {
+//   yesterday: 75,
+//   today: 77,
+//   tomorrow: 80,
+// };
+
+// const { today, tomorrow } = HIGH_TEMPERATURES;
+
+// const HIGH_TEMPERATURES = {
+//   yesterday: 75,
+//   today: 77,
+//   tomorrow: 80,
+// };
+
+// const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
+// Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables lowToday and highToday the values of today.low and today.high from the LOCAL_FORECAST object
+
+// const LOCAL_FORECAST = {
+//   yesterday: { low: 61, high: 75 },
+//   today: { low: 64, high: 77 },
+//   tomorrow: { low: 68, high: 80 },
+// };
+
+// const {
+//   today: { low: lowToday, high: highToday },
+// } = LOCAL_FORECAST;
+// console.log("lowTodat", lowToday);
+// console.log("highToday", highToday);
