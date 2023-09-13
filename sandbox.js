7c564c251e491237ef80d550c14f8bd6383a9e13
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-09-13 20:50:13 +0300

    end basic data

--- sandbox.js
+++ sandbox.js
@@ -726,3 +726,304 @@
 //     console.log("Still going!");
 //   }
 // }
+
+// * Basic Data Structures
+
+//! We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+// const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+
+// console.log(arr.splice(1, 4));
+
+// console.log(arr);
+
+//! We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. Modify the function using splice() to remove the first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in their respective places.
+
+// function htmlColorNames(arr) {
+
+//   arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+
+//   return arr;
+// }
+
+// console.log(
+//   htmlColorNames(["DarkGoldenRod", "WhiteSmoke", "LavenderBlush", "PaleTurquoise", "FireBrick"])
+// );
+
+//! We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the string elements warm and sunny.
+// function forecast(arr) {
+//   // Only change code below this line
+//   const newArr = arr.slice(2, 4);
+//   return newArr;
+// }
+
+// // Only change code above this line
+// console.log(forecast(["cold", "rainy", "warm", "sunny", "cool", "thunderstorms"]));
+
+//! We have defined a function, copyMachine which takes arr (an array) and num (a number) as arguments. The function is supposed to return a new array made up of num copies of arr. We have done most of the work for you, but it doesn't work quite right yet. Modify the function using spread syntax so that it works correctly (hint: another method we have already covered might come in handy here!).
+// function copyMachine(arr, num) {
+//   let newArr = [];
+//   while (num >= 1) {
+//     // Only change code below this line
+//     newArr.push([...arr]);
+//     // Only change code above this line
+//     num--;
+//   }
+//   return newArr;
+// }
+
+// console.log(copyMachine([true, false, true], 2));
+
+//! We have defined a function spreadOut that returns the variable sentence. Modify the function using the spread operator so that it returns the array ['learning', 'to', 'code', 'is', 'fun'].
+
+// function spreadOut() {
+//   let fragment = ["to", "code"];
+//   let sentence = ["learning", ...fragment, "is", "fun"]; // Change this line
+//   return sentence;
+// }
+
+// console.log(spreadOut());
+
+//! indexOf() can be incredibly useful for quickly checking for the presence of an element on an array. We have defined a function, quickCheck, that takes an array and an element as arguments. Modify the function using indexOf() so that it returns true if the passed element exists on the array, and false if it does not.
+// function quickCheck(arr, elem) {
+//   if (arr.indexOf(elem) === -1) {
+//     return false;
+//   }
+//   return true;
+// }
+
+// console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
+
+//! We have defined a function, filteredArray, which takes arr, a nested array, and elem as arguments, and returns a new array. elem represents an element that may or may not be present on one or more of the arrays nested within arr. Modify the function, using a for loop, to return a filtered version of the passed array such that any array nested within arr containing elem has been removed.
+
+// function filteredArray(arr, elem) {
+//   let newArr = [];
+//   // Only change code below this line
+//   for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+
+//     if (element.indexOf(elem) === -1) {
+//       newArr.push(element);
+//     }
+//   }
+//   // Only change code above this line
+//   return newArr;
+// }
+
+// console.log(
+//   filteredArray(
+//     [
+//       [3, 2, 3],
+//       [1, 6, 3],
+//       [3, 13, 26],
+//       [19, 3, 9],
+//       [19, 5, 9],
+//     ],
+//     3
+//   )
+// );
+
+//! We have defined a variable, myNestedArray, set equal to an array. Modify myNestedArray, using any combination of strings, numbers, and booleans for data elements, so that it has exactly five levels of depth (remember, the outer-most array is level 1). Somewhere on the third level, include the string deep, on the fourth level, include the string deeper, and on the fifth level, include the string deepest.
+
+// let myNestedArray = [
+//   // Only change code below this line
+//   ["unshift", false, 1, 2, 3, "complex", "nested"],
+//   [
+//     ["loop", "shift", 6, 7, 1000, "deep"],
+//     [
+//       ["mutate", 1327.98, "splice", "slice", "deeper"],
+//       [["iterate", 1.3849, 7, "8.4876", "arbitrary", "deepest"]],
+//     ],
+//   ],
+//   ["concat", false, true, "spread", "hi"],
+
+//   // Only change code above this line
+// ];
+
+// console.log(myNestedArray);
+
+//! A foods object has been created with three entries. Using the syntax of your choice, add three more entries to it: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.
+// let foods = {
+//   apples: 25,
+//   oranges: 32,
+//   plums: 28,
+// };
+
+// // Only change code below this line
+// foods.bananas = 13;
+// foods.grapes = 35;
+// foods.strawberries = 27;
+// // Only change code above this line
+
+// console.log(foods);
+
+//! Here we've defined an object userActivity, which includes another object nested within it. Set the value of the online key to 45.
+
+// let userActivity = {
+//   id: 23894201352,
+//   date: "January 1, 2017",
+//   data: {
+//     totalUsers: 51,
+//     online: 42,
+//   },
+// };
+
+// // Only change code below this line
+// userActivity.data.online = 45;
+// // Only change code above this line
+
+// console.log(userActivity);
+
+//! We've defined a function, checkInventory, which receives a scanned item as an argument. Return the current value of the scannedItem key in the foods object. You can assume that only valid keys will be provided as an argument to checkInventory.
+// let foods = {
+//   apples: 25,
+//   oranges: 32,
+//   plums: 28,
+//   bananas: 13,
+//   grapes: 35,
+//   strawberries: 27,
+// };
+
+// function checkInventory(scannedItem) {
+//   // Only change code below this line
+//   return foods[scannedItem];
+//   // Only change code above this line
+// }
+
+// console.log(checkInventory("apples"));
+
+//! Use the delete keyword to remove the oranges, plums, and strawberries keys from the foods object.
+// let foods = {
+//   apples: 25,
+//   oranges: 32,
+//   plums: 28,
+//   bananas: 13,
+//   grapes: 35,
+//   strawberries: 27,
+// };
+
+// // Only change code below this line
+// delete foods.oranges;
+// delete foods.plums;
+// delete foods.strawberries;
+// // Only change code above this line
+
+// console.log(foods);
+
+//! Finish writing the function so that it returns true if the object passed to it contains all four names, Alan, Jeff, Sarah and Ryan and returns false otherwise.
+
+// let users = {
+//   Alan: {
+//     age: 27,
+//     online: true,
+//   },
+//   Jeff: {
+//     age: 32,
+//     online: true,
+//   },
+//   Sarah: {
+//     age: 48,
+//     online: true,
+//   },
+//   Ryan: {
+//     age: 19,
+//     online: true,
+//   },
+// };
+
+// function isEveryoneHere(userObj) {
+//   if (
+//     userObj.hasOwnProperty("Alan") &&
+//     userObj.hasOwnProperty("Jeff") &&
+//     userObj.hasOwnProperty("Sarah") &&
+//     userObj.hasOwnProperty("Ryan")
+//   ) {
+//     return true;
+//   }
+// }
+
+// console.log(isEveryoneHere(users));
+
+//! We've defined a function countOnline which accepts one argument, allUsers. Use a for...in statement inside this function to loop through the allUsers object and return the number of users whose online property is set to true. An example of an object which could be passed to countOnline is shown below. Each user will have an online property set to either true or false.
+// const users = {
+//   Alan: {
+//     online: false,
+//   },
+//   Jeff: {
+//     online: true,
+//   },
+//   Sarah: {
+//     online: false,
+//   },
+// };
+
+// function countOnline(allUsers) {
+//   // Only change code below this line
+//   let total = 0;
+
+//   for (const user in allUsers) {
+//     if (allUsers[user].online) {
+//       total += 1;
+//     }
+//   }
+//   return total;
+//   // Only change code above this line
+// }
+
+// console.log(countOnline(users));
+
+//! Finish writing the getArrayOfUsers function so that it returns an array containing all the properties in the object it receives as an argument.
+// let users = {
+//   Alan: {
+//     age: 27,
+//     online: false,
+//   },
+//   Jeff: {
+//     age: 32,
+//     online: true,
+//   },
+//   Sarah: {
+//     age: 48,
+//     online: false,
+//   },
+//   Ryan: {
+//     age: 19,
+//     online: true,
+//   },
+// };
+
+// function getArrayOfUsers(obj) {
+//   return Object.keys(obj);
+// }
+
+// console.log(getArrayOfUsers(users));
+
+// !The user object contains three keys.The data key contains five keys, one of which contains an array of friends.From this, you can see how flexible objects are as data structures.We've started writing a function addFriend. Finish writing it so that it takes a user object and adds the name of the friend argument to the array stored in user.data.friends and returns that array.
+
+// let user = {
+//   name: "Kenneth",
+//   age: 28,
+//   data: {
+//     username: "kennethCodesAllDay",
+//     joinDate: "March 26, 2016",
+//     organization: "freeCodeCamp",
+//     friends: ["Sam", "Kira", "Tomo"],
+//     location: {
+//       city: "San Francisco",
+//       state: "CA",
+//       country: "USA",
+//     },
+//   },
+// };
+
+// function addFriend(userObj, friend) {
+//   // Only change code below this line
+//   const {
+//     data: { friends },
+//   } = userObj;
+
+//   friends.push(friend);
+//   return friends;
+//   // Only change code above this line
+// }
+
+// console.log(addFriend(user, "Pete"));
