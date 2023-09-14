88a2b76d7c7d8ecfe76e9abfeb6aced40785fadf
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-09-14 17:29:38 +0300

    start Basic Algorithm Scripting  part 1

--- sandbox.js
+++ sandbox.js
@@ -1027,3 +1027,119 @@
 // }
 
 // console.log(addFriend(user, "Pete"));
+
+// * Basic Algorithm Scripting
+
+// !The formula to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+//! You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the formula mentioned above to help convert the Celsius temperature to Fahrenheit.
+// function convertCtoF(celsius) {
+//   let fahrenheit = (celsius * 9) / 5 + 32;
+//   return fahrenheit;
+// }
+
+// console.log(convertCtoF(30));
+
+// !Reverse the provided string and return the reversed string.
+// !For example, "hello" should become "olleh".
+// function reverseString(str) {
+//   let splitedStr = str.split("");
+//   let newString = [];
+
+//   for (let i = 0; i < splitedStr.length; i++) {
+//     const element = splitedStr[i];
+//     newString.unshift(element);
+//   }
+
+//   return newString.join("");
+// }
+
+// console.log(reverseString("hello"));
+
+// !Return the factorial of the provided integer.
+//! If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.Factorials are often represented with the shorthand notation n!
+//! For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+//! Only integers greater than or equal to zero will be supplied to the function.
+
+// function factorialize(num) {
+//   let factorial = 1;
+//   while (num > 0) {
+//     factorial *= num;
+
+//     num -= 1;
+//   }
+//   return factorial;
+// }
+
+// console.log(factorialize(5));
+
+//! Return the length of the longest word in the provided sentence.
+//! Your response should be a number.
+
+// function findLongestWordLength(str) {
+//   let splitedStr = str.split(" ");
+//   let longestWord = splitedStr[0];
+
+//   for (const item of splitedStr) {
+//     if (item.length > longestWord.length) {
+//       longestWord = item;
+//     }
+//   }
+
+//   return longestWord.length;
+// }
+
+// console.log(findLongestWordLength("The quick brown fox jumped bababababbabab over the lazy dog"));
+
+//! Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+//! Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+// function largestOfFour(arr) {
+//   let result = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+
+//     result.push(Math.max(...element));
+//   }
+//   return result;
+// }
+
+// console.log(
+//   largestOfFour([
+//     [4, 5, 1, 3],
+//     [13, 27, 18, 26],
+//     [32, 35, 37, 39],
+//     [1000, 1001, 857, 1],
+//   ])
+// );
+
+//! Check if a string (first argument, str) ends with the given target string (second argument, target).
+//! This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+// function confirmEnding(str, target) {
+//   let targetLength = target.length;
+//   let targetMatch = str.slice(str.length - targetLength, str.length);
+
+//   //   let array = str.split(" ");
+//   //   let lastWord = array[array.length - 1];
+
+//   if (targetMatch === target) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(confirmEnding("Bastian", "n"));
+
+//! Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+// function repeatStringNumTimes(str, num) {
+//   let result = [];
+//   while (num > 0) {
+//     result.push(str);
+
+//     num -= 1;
+//   }
+//   return result.join("");
+// }
+
+// console.log(repeatStringNumTimes("abc", 3));
